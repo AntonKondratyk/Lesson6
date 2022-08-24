246df53001f5b87badb8dd2de3a71111f04da6d7
Anton Kondratyk <andeev1910@mail.ru>
2022-08-24 20:47:25 +0500

    фйцу

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -12,12 +12,27 @@ namespace Task2
         {
             string str = Console.ReadLine();
             string str2 = "";
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                str2 += str[i];
+            }
+            if (str == str2)
+            {
+                Console.WriteLine("палиндром");
+            }
+            else
+            {
+                Console.WriteLine("не палиндром");
+            }
+            Console.ReadKey();
+            /*string str = Console.ReadLine();
+            string str2 = "";
             foreach (char c in str)
             {
                 str2 = c + str2;
             }
             Console.WriteLine(str2);
-            Console.ReadKey();
+            Console.ReadKey();*/
         }
     }
 }
